0b6365fb523244e9caedebccb4ab5f4ccdc62723
ilya shusterman <ilyashusterman@outlook.com>
2017-11-23 22:15:43 +0200

    refactored search test

--- describers/test/TestDescriptor.py
+++ describers/test/TestDescriptor.py
@@ -1,15 +1,7 @@
 from unittest import TestCase
 
-from describers.Searcher import Searcher
-
 
 class TestDescriptor(TestCase):
     def setUp(self):
         with open('test_image.png') as data_file:
             self.test_image = data_file
-
-    def test_keyword_search(self):
-        keyword = 'dog'
-        s = Searcher(keyword)
-        analization = s.get_analyzization()
-        self.assertEqual(analization['name'], 'dog')

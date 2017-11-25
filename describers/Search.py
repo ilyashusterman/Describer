d3790f65af600428d40255745f43eb03dec1e0f4
ilya shusterman <ilyashusterman@outlook.com>
2017-11-25 15:16:57 +0200

    added test of 3 sentences

--- describers/Search.py
+++ describers/Search.py
@@ -6,7 +6,7 @@ class Search(object):
     def __init__(self, keyword):
         self.keyword = keyword
 
-    def get_analyzization(self):
+    def get_analyzization(self, sentences=2):
         # citi_found_pages = citizendium.search(self.keyword)
         found_pages = wikipedia.search(self.keyword)
         if len(found_pages) > 0:
@@ -17,7 +17,7 @@ class Search(object):
         else:
             return {'name': 'no_information'}
         # value_meta = wikipedia.page(value_found)
-        summary = wikipedia.summary(value_found, sentences=2)
+        summary = wikipedia.summary(value_found, sentences=sentences)
         # summary_city = citizendium.summary('Dog', sentences=2)
 
         return {
--- describers/test/TestSearch.py
+++ describers/test/TestSearch.py
@@ -22,3 +22,22 @@ class TestDescriptor(TestCase):
         self.assertEqual(analization.get('name'), 'dog')
         self.assertEqual(len(analization.get('summary')), len(summary),
                          analization)
+
+    def test_analization_summary_of_3_sentences(self):
+        summary = 'A smartphone is a handheld personal computer with a ' \
+                  'mobile operating system and an integrated mobile' \
+                  ' broadband cellular network connection for voice, ' \
+                  'SMS, and Internet data communication; most if not' \
+                  ' all smartphones also support Wi-Fi. Smartphones ' \
+                  'are typically pocket-sized, as opposed to tablets, ' \
+                  'which are much larger than a pocket. ' \
+                  'They are able to run a variety of ' \
+                  'third-party software components ("apps") ' \
+                  'from places like the Google Play Store or Apple App ' \
+                  'Store, and can receive bug fixes and gain additional ' \
+                  'functionality through operating system software updates.'
+
+        search = Search('smartphone')
+        analization = search.get_analyzization(sentences=3)
+        self.assertEqual(len(analization['summary']), len(summary),
+                         analization)

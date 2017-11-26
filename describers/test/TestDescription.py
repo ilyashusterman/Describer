1fdea24530276ce3c122978ab40e82e1a49f690b
Ilya Shusterman <ilya@allaspect.com>
2017-11-26 14:09:28 +0200

    refactored tests

--- describers/test/TestDescription.py
+++ describers/test/TestDescription.py
@@ -6,13 +6,8 @@ from describers.Search import Search
 
 
 class TestDescription(TestCase):
-
     def test_description_of_desk_image(self):
         item = 'desk'
-        recognition = Recognition(Path(Path(__file__).parent, 'test_{}.jpg'.format(item)))
-        classification = recognition.classify_image()
-        self.assertTrue('{}'.format(item) in classification['value'],
-                        classification)
         summary = 'A desk or bureau is a piece of furniture with a flat ' \
                   'table-style work surface used in a school, office, ' \
                   'home or the like for academic, professional or domestic' \
@@ -21,41 +16,33 @@ class TestDescription(TestCase):
                   'more drawers, ' \
                   'compartments, or pigeonholes to store items' \
                   ' such as office supplies and papers.'
-        search = Search(classification['value'])
-        analization = search.get_analyzization(sentences=2)
-        self.assertEqual(len(analization['summary']), len(summary),
-                         analization)
+        self.check_image(item, summary)
 
     def test_description_of_door_image(self):
         item = 'door'
-        recognition = Recognition(Path(Path(__file__).parent,
-                                       'test_{}.jpg'.format(item)))
-        classification = recognition.classify_image()
-        self.assertTrue('{}'.format(item) in classification['value'],
-                        classification)
         summary = 'A door is a moving mechanism used to block off,' \
                   ' and allow access to, an entrance to or within ' \
                   'an enclosed space, such as a building,' \
                   ' room or vehicle. Doors normally consist ' \
                   'of one or two solid panels, with or without' \
                   ' windows, that swing on hinges horizontally.'
-        search = Search(classification['value'])
-        analization = search.get_analyzization(sentences=2)
-        self.assertEqual(len(analization['summary']), len(summary),
-                         analization)
+        self.check_image(item, summary)
 
     def test_description_of__image(self):
         item = 'bottle'
-        recognition = Recognition(Path(Path(__file__).parent,
-                                       'test_{}.jpg'.format(item)))
-        classification = recognition.classify_image()
-        self.assertTrue('{}'.format(item) in classification['value'],
-                        classification)
         summary = 'A bottle is a rigid container with a neck' \
                   ' that is narrower than the body and a mouth. ' \
                   'By contrast, a jar has a relatively large ' \
                   'mouth or opening which may be as wide as' \
                   ' the overall container.'
+        self.check_image(item, summary)
+
+    def check_image(self, item, summary):
+        recognition = Recognition(Path(Path(__file__).parent,
+                                       'test_{}.jpg'.format(item)))
+        classification = recognition.classify_image()
+        self.assertTrue('{}'.format(item) in classification['value'],
+                        classification)
         search = Search(classification['value'])
         analization = search.get_analyzization(sentences=2)
         self.assertEqual(len(analization['summary']), len(summary),

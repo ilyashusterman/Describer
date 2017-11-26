c1126360b72ff8b77c09bfdf2f0a90ba8cac4be1
Ilya Shusterman <ilya@allaspect.com>
2017-11-26 10:12:00 +0200

    fixed tests , added requirements.txt

--- describers/test/TestSearch.py
+++ describers/test/TestSearch.py
@@ -9,14 +9,15 @@ class TestDescriptor(TestCase):
 
     def test_keyword_search(self):
         keyword = 'dog'
-        summary = 'The domestic dog (Canis lupus familiaris or' \
-                  ' Canis familiaris) is a wolf-like canid and a' \
-                  ' member of the genus Canis, and is the most abundant ' \
-                  'carnivore. The dog and the extant gray wolf are sister ' \
-                  'taxa as modern wolves are not closely related to ' \
-                  'the wolves that were first domesticated, ' \
-                  'which implies that the direct ancestor of ' \
-                  'the dog is extinct.'
+        summary = 'The domestic dog (Canis lupus familiaris' \
+                  ' or Canis familiaris) is a wolf-like canid' \
+                  ' in the genus Canis, and is the most widely' \
+                  ' abundant terrestrial carnivore. The dog and ' \
+                  'the extant gray wolf are sister taxa ' \
+                  'as modern wolves are not closely related ' \
+                  'to the wolves that were first domesticated,' \
+                  ' which implies that the direct ancestor ' \
+                  'of the dog is extinct.'
         s = Search(keyword)
         analization = s.get_analyzization()
         self.assertEqual(analization.get('name'), 'dog')

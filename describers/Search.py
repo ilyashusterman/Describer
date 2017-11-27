43a4f185c8759360d6ced56221945afd0e129ddb
Ilya Shusterman <ilya@allaspect.com>
2017-11-27 10:06:49 +0200

    refactor search found values

--- describers/Search.py
+++ describers/Search.py
@@ -11,15 +11,12 @@ class Search(object):
     def get_analyzization(self, sentences=2):
         found_pages = wikipedia.search(self.keyword)
         if len(found_pages) > 0:
-            value_found = found_pages[0]
-            # citi_value_found = citi_found_pages[0]
+            default_value = found_pages[0]
             logging.debug('found_pages={}'.format(found_pages))
-            # ‎TODO need to refactor assertion
-            assert value_found == self.keyword.title() or \
-                   value_found in self.keyword or \
-                   self.keyword in value_found
+            value_found = self.get_found_value(found_pages, default_value)
         else:
-            return {'name': 'no_information'}
+            return {'name': 'no_information',
+                    'summary': 'no_information'}
         # value_meta = wikipedia.page(value_found)
         summary = wikipedia.summary(value_found, sentences=sentences)
         return {
@@ -27,6 +24,16 @@ class Search(object):
             'summary': str(summary)
         }
 
+    def get_found_value(self, found_values, value_found):
+        found_value = value_found
+        for keyword_value in found_values:
+            if keyword_value == self.keyword.title() or \
+                            keyword_value in self.keyword or \
+                            self.keyword in keyword_value:
+                found_value = keyword_value
+                break
+        return found_value
+
     def get_source_analization(self, sentences=2):
-        #TODO
+        # TODO
         pass
